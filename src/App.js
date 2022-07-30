8a5fcc307f8ee4bf5ec3a1c8bb9e3bd2b77d9d3b
Gilang Hadi <gilangsaputro987@gmail.com>
2022-07-30 19:55:58 +0700

    try tailwind.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from '../src/Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className='text-3xl font-bold underline decoration-sky-500'>
-          hello world
-        </h1>
-      </header>
+      <div className='nav'>
+            <Navbar />
+      </div>
     </div>
   );
 }
